9cf007407a4e2c13e3e915cc5c502283d0a58ee3
imisgsc <simigracia@gmail.com>
2024-06-06 11:54:14 +0530

    Completed

---
+++ TASK-2/script.js
@@ -0,0 +1,58 @@
+let display = document.getElementById('display');
+let currentOperand = '';
+let previousOperand = '';
+let operation = null;
+
+function appendNumber(number) {
+    if (currentOperand.includes('.') && number === '.') return;
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function setOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        calculate();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function calculate() {
+    let result;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = result;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = null;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.value = currentOperand;
+}
