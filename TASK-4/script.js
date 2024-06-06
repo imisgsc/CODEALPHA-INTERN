78332c08222802a58637cdbedc72f2b3eb65483c
imisgsc <simigracia@gmail.com>
2024-06-06 19:27:14 +0530

    initiated task 4

---
+++ TASK-4/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const audioPlayer = document.getElementById('audio-player');
+    const playBtn = document.getElementById('play-btn');
+    const pauseBtn = document.getElementById('pause-btn');
+    const stopBtn = document.getElementById('stop-btn');
+
+    playBtn.addEventListener('click', () => {
+        audioPlayer.play();
+    });
+
+    pauseBtn.addEventListener('click', () => {
+        audioPlayer.pause();
+    });
+
+    stopBtn.addEventListener('click', () => {
+        audioPlayer.pause();
+        audioPlayer.currentTime = 0;
+    });
+});
